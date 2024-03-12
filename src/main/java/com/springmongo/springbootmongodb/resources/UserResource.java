ee1f9f8609d625d81dce6fc27b0c0aee500e81da
Wenio Santana <wenio.souza123@gmail.com>
2024-03-12 11:01:57 -0300

    Adicionando Insert

--- src/main/java/com/springmongo/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/springmongo/springbootmongodb/resources/UserResource.java
@@ -1,17 +1,18 @@
 package com.springmongo.springbootmongodb.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.springmongo.springbootmongodb.domain.User;
 import com.springmongo.springbootmongodb.domain.UserDTO;
@@ -41,5 +42,18 @@ public class UserResource {
 		return ResponseEntity.ok().body(new UserDTO(list));
 
 	}
+	
+	@PostMapping
+	public ResponseEntity<Void> Insert(@RequestBody UserDTO objDTO){
+		
+		User obj = service.fromDTO(objDTO);
+		obj = service.Insert(obj);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+		
+	}
+	
 	
 }
--- src/main/java/com/springmongo/springbootmongodb/services/UserService.java
+++ src/main/java/com/springmongo/springbootmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springmongo.springbootmongodb.domain.User;
+import com.springmongo.springbootmongodb.domain.UserDTO;
 import com.springmongo.springbootmongodb.repository.UserRepository;
 import com.springmongo.springbootmongodb.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,14 @@ public class UserService {
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+	
+	public User Insert(User obj) {
+		
+		return repo.insert(obj);
+	}
 
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
+	
 }
