68959e039faf24ba7baa26dac54267f17b3b970d
Wenio Santana <wenio.souza123@gmail.com>
2024-03-18 16:43:47 -0300

    Adicionando Autor dos post

--- src/main/java/com/springmongo/springbootmongodb/config/Instantion.java
+++ src/main/java/com/springmongo/springbootmongodb/config/Instantion.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.springmongo.springbootmongodb.domain.AuthorDTO;
 import com.springmongo.springbootmongodb.domain.Post;
 import com.springmongo.springbootmongodb.domain.User;
 import com.springmongo.springbootmongodb.repository.PostRepository;
@@ -37,8 +38,8 @@ public class Instantion implements CommandLineRunner{
 
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));;
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
