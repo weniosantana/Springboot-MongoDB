f8241ed520a7cb8f7dc62f2b7d4766e320246c6e
Wenio Santana <wenio.souza123@gmail.com>
2024-04-01 15:08:03 -0300

    Adicionando alternativa para pesquisa com @Query

--- src/main/java/com/springmongo/springbootmongodb/repository/PostRepository.java
+++ src/main/java/com/springmongo/springbootmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.springmongo.springbootmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.springmongo.springbootmongodb.domain.Post;
@@ -10,6 +11,11 @@ import com.springmongo.springbootmongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	
+	
+	//Alternativa
+	@Query("{ 'title': { $regex: ?0, $options: 'i' }}")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByBodyContainingIgnoreCase(String text);
 }
